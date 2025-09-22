3958e56271705e43f16432b5d32699ea98d6e5a6
habbababbai <qba.7145@gmail.com>
2025-09-22 18:58:48 +0200

    apply styling to login page

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,3 +1,5 @@
 export const colors = {
     bg: "#8D99AE",
+    white: "#FFFFFF",
+    button: "#2B2D42",
 };
--- utils/responsive.ts
+++ utils/responsive.ts
@@ -5,30 +5,30 @@ const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 export const FIGMA_DESIGN_WIDTH = 393;
 export const FIGMA_DESIGN_HEIGHT = 852;
 
-export const scale = (size: number): number => {
-    return (screenWidth / FIGMA_DESIGN_WIDTH) * size;
+export const fp = (figmaFontSize: number): number => {
+    const scaledSize = (screenWidth / FIGMA_DESIGN_WIDTH) * figmaFontSize;
+    return Math.max(12, PixelRatio.roundToNearestPixel(scaledSize));
 };
 
-export const scaleHeight = (size: number): number => {
-    return (screenHeight / FIGMA_DESIGN_HEIGHT) * size;
+export const wp = (figmaWidth: number): number => {
+    return (screenWidth / FIGMA_DESIGN_WIDTH) * figmaWidth;
 };
 
-export const scaleFont = (size: number): number => {
-    const newSize = scale(size);
-    return Math.max(12, PixelRatio.roundToNearestPixel(newSize));
+export const hp = (figmaHeight: number): number => {
+    return (screenHeight / FIGMA_DESIGN_HEIGHT) * figmaHeight;
 };
 
-export const wp = (percentage: number): number => {
-    return (screenWidth * percentage) / 100;
+export const sp = (figmaSpacing: number): number => {
+    return (screenWidth / FIGMA_DESIGN_WIDTH) * figmaSpacing;
 };
 
-export const hp = (percentage: number): number => {
-    return (screenHeight * percentage) / 100;
-};
-
-export const getDeviceDimensions = () => ({
-    width: screenWidth,
-    height: screenHeight,
-    figmaWidth: FIGMA_DESIGN_WIDTH,
-    figmaHeight: FIGMA_DESIGN_HEIGHT,
-});
+export const spacing = {
+    xxs: sp(4),
+    xs: sp(8),
+    sm: sp(12),
+    md: sp(16),
+    lg: sp(20),
+    xl: sp(24),
+    xxl: sp(32),
+    xxxl: sp(40),
+} as const;
